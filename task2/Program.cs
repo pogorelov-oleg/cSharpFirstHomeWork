7aed8058cffcd780cccbb7457590cf2adaeee8f1
pogorelov <89607888555@mail.ru>
2023-04-08 03:33:45 +0700

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -2,8 +2,14 @@
 string userInput1 = Console.ReadLine() ?? "";
 Console.Write ("Введите второе число: ");
 string userInput2 = Console.ReadLine() ?? "";
-int userNumbe1 = int.Parse(userInput1);
-int userNumbe2 = int.Parse(userInput2);
-
-
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+if (userNumber1 > userNumber2)
+{
+    Console.WriteLine($"Число {userNumber1} больше {userNumber2}");
+}
+else 
+{
+    Console.WriteLine($"Число {userNumber1} меньше {userNumber2}");
+}
 
