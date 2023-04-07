48baa2faff549dcd17e748a7796f6f92747700ca
pogorelov <89607888555@mail.ru>
2023-04-08 03:58:38 +0700

    task4 done

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿Console.Write ("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write ("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+Console.Write ("Введите третье число: ");
+string userInput3 = Console.ReadLine() ?? "";
+int userNumber1 = int.Parse(userInput1);
+int userNumber2 = int.Parse(userInput2);
+int userNumber3 = int.Parse(userInput3);
+int maxNumber;
+if (userNumber2 > userNumber1)
+{
+    maxNumber = userNumber2;
+}
+else 
+{
+    maxNumber = userNumber1;
+}
+if (userNumber3 > maxNumber)
+{
+    Console.Write ($"Максимальное число {userNumber3}");
+}
+else
+{
+    Console.Write ($"Максимальное число {maxNumber}");
+}
