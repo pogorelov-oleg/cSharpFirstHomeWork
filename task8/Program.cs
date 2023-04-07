26157ad3c2d89ecbc6cb5c1cab0c4d8b068d0429
pogorelov <89607888555@mail.ru>
2023-04-08 05:18:06 +0700

    task8 done

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if (userNumber < 2)
+{
+    Console.Write($"От 0 до {userNumber} нет четных чисел");
+}
+else
+{
+    for (int i = 2; i <= userNumber; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i} ");
+            i++;
+        }
+    }
+}
