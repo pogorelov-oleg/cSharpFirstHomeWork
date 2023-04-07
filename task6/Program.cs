3bc9c5e23187ac6e55ec9baf480ca21efdd2c869
pogorelov <89607888555@mail.ru>
2023-04-08 04:26:30 +0700

    task6 done

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿Console.Write ("Введите первое число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+if (userNumber % 2 == 0)
+{
+    Console.Write($"Число {userNumber} является четным");
+}
+else
+{
+    Console.Write($"Число {userNumber} является нечетным");
+}
