7844065afae84ae09f01dfc2569711a3332abdc1
pogorelov <89607888555@mail.ru>
2023-04-08 03:25:41 +0700

    added README file

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿Console.Write ("Введите первое число: ");
+string userInput1 = Console.ReadLine() ?? "";
+Console.Write ("Введите второе число: ");
+string userInput2 = Console.ReadLine() ?? "";
+int userNumbe1 = int.Parse(userInput1);
+int userNumbe2 = int.Parse(userInput2);
+
+
+
